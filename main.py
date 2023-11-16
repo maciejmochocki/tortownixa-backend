3323eaac1223e0e997cc79b29fd654abc790b335
Adrian Chmielowiec <adikx107@gmail.com>
2023-11-16 19:01:00 +0100

    add class scene

--- main.py
+++ main.py
@@ -1,39 +1,63 @@
 import os
+
+from OCC.Core.BRep import BRep_Builder
 from OCC.Core.BRepPrimAPI import BRepPrimAPI_MakeBox, BRepPrimAPI_MakeTorus
+from OCC.Core.TopoDS import TopoDS_Compound
 from OCC.Extend.DataExchange import write_stl_file, read_stl_file
 from OCC.Display.SimpleGui import init_display
 
+
+class Scene:
+    def __init__(self):
+        self.objects = []
+
+    def add_object(self, obj):
+        self.objects.append(obj)
+
+    def remove_object(self, obj):
+        self.objects.remove(obj)
+
+    def export_to_stl(self, path):
+        # deklaracja pustego obiektu
+        compound = TopoDS_Compound()
+        BRep_Builder().MakeCompound(compound)
+
+        # dodaj wszystkie obiekty na scenie
+        for obj in self.objects:
+            BRep_Builder().Add(compound, obj)
+
+        # Zapisz do pliku STL
+        write_stl_file(compound, path, mode="binary", linear_deflection=0.5, angular_deflection=0.3)
+
+
 # Inicjalizacja wyświetlania
 display, start_display, add_menu, add_function_to_menu = init_display()
-
-# ustawienie folderu do eksportu obiektów
-stl_output_dir = os.path.abspath(os.path.join("assets", "models"))
 
 # Tworzenie obiektów
 box = BRepPrimAPI_MakeBox(10, 20, 30).Shape()
 my_torus = BRepPrimAPI_MakeTorus(20.0, 10.0).Shape()
 
-# sprawdzenie sciezki
-if not os.path.isdir(stl_output_dir):
-    raise AssertionError("wrong path provided")
+# Tworzenie sceny
+scene = Scene()
 
-# ustawienie nazwy dla pliku
-stl_file_torus = os.path.join(stl_output_dir, "torus.stl")
+# Dodanie obiektow do sceny
+scene.add_object(box)
+scene.add_object(my_torus)
 
-# eskport do pliku
-write_stl_file(
-    my_torus,
-    stl_file_torus,
-    mode="binary",
-    linear_deflection=0.5,
-    angular_deflection=0.3,
-)
+# Usun box z sceny
+scene.remove_object(box)
 
-# Wczytywanie STL
-stl_torus = read_stl_file(stl_file_torus)
+# sciezka do pliku sceny
+stl_output = "assets/models/scene.stl"
+
+# Zapisanie sceny do pliku
+scene.export_to_stl(stl_output)
+
+# Wczytanie sceny i wyswietlenie
+stl_scene = read_stl_file(stl_output)
 
 # Wyświetlenie wczytanego pudełka
-display.DisplayShape(stl_torus, update=True, color="RED")
+display.DisplayShape(stl_scene, update=True, color="RED")
 
 # Uruchomienie interaktywnego widoku
 start_display()
